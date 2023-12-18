c7917481af4b099c04777acf3e5865dc08e126ce
alexraduca01 <alex.raduca01@icloud.com>
2023-12-18 15:54:42 +0100

    added footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,52 @@
 <footer>
-    Footer 
+    <div class="container d-flex p-0 align-items-center position-relative py-5">
+        <div class="col-4 d-flex gap-4">
+            <div>
+                <ul class="m-0">
+                    <li class="text-uppercase mb-2"><h5 class="text-white">dc comics</h5></li>
+                    <li class="my-font-size">Characters</li>
+                    <li class="my-font-size">Comics</li>
+                    <li class="my-font-size">Movies</li>
+                    <li class="my-font-size">TV</li>
+                    <li class="my-font-size">Games</li>
+                    <li class="my-font-size">Videos</li>
+                    <li class="my-font-size">News</li>
+                </ul>
+                <ul class="m-0 mt-5">
+                    <li class="text-uppercase mb-2"><h5 class="text-white">shop</h5></li>
+                    <li class="my-font-size">Shop DC</li>
+                    <li class="my-font-size">Shop DC Collectibles</li>
+                </ul>
+            </div>
+            <div>
+                <ul class="m-0">
+                    <li class="text-uppercase mb-2"><h5 class="text-white">dc</h5></li>
+                    <li class="my-font-size">Terms Of Use</li>
+                    <li class="my-font-size">Privacy policy (New)</li>
+                    <li class="my-font-size">Ad Choices</li>
+                    <li class="my-font-size">Advertising</li>
+                    <li class="my-font-size">Jobs</li>
+                    <li class="my-font-size">Subscriptions</li>
+                    <li class="my-font-size">Talent Workshops</li>
+                    <li class="my-font-size">CPSC Certificates</li>
+                    <li class="my-font-size">Ratings</li>
+                    <li class="my-font-size">Shop Help</li>
+                    <li class="my-font-size">Contact Us</li>
+                </ul>
+            </div>
+            <div>
+                <ul class="m-0">
+                    <li class="text-uppercase mb-2"><h5 class="text-white">sites</h5></li>
+                    <li class="my-font-size">DC</li>
+                    <li class="my-font-size">MAD Magazine</li>
+                    <li class="my-font-size">DC Kids</li>
+                    <li class="my-font-size">DC Universe</li>
+                    <li class="my-font-size">DC Power Visa</li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-8 d-flex justify-content-end ">
+            <img src="img/dc-logo-bg.png" class="footer-img" alt="big-logo">
+        </div>
+    </div>
 </footer>

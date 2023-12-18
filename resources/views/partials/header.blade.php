0f91e8ed224a549464f3464c5f89283c3d3b4f1b
alexraduca01 <alex.raduca01@icloud.com>
2023-12-18 14:31:24 +0100

    added header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,45 @@
 <header>
-    Header <i class="fa-solid fa-house"></i>
+    <div class="container d-flex justify-content-between align-items-center p-3">
+        <div>
+            <img src="img/dc-logo.png" alt="logo">
+        </div>
+        <nav>
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link text-black text-uppercase" href="#">characters</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-black text-uppercase" href="#">comics</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-black text-uppercase" href="#">movies</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-black text-uppercase" href="#">tv</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-black text-uppercase" href="#">games</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-black text-uppercase" href="#">collectibles</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-black text-uppercase" href="#">videos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-black text-uppercase" href="#">fans</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-black text-uppercase" href="#">news</a>
+                </li>
+            </ul>
+        </nav>
+        <div class="input-group w-auto ">
+            <span class="input-group-text"><i class="fa-solid fa-magnifying-glass"></i></span>
+            <div class="form-floating">
+                <input type="text" class="form-control" id="floatingInputGroup1" placeholder="Search">
+                <label for="floatingInputGroup1">Search</label>
+            </div>
+        </div>
+    </div>
 </header>
